10f8679c2a70ffb1e1632d7c315e3b6b4dc13ec4
Jeeva Krishna <88268605+Jeevakrishna@users.noreply.github.com>
2021-09-14 21:51:20 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,379 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+     {
+    numb: 1,
+    question: "Je voudrais ............ rendez-vous avec le docteur.?",
+    answer: "un",
+    options: [
+      "une",
+      "un",
+      "de",
+      "des"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Elle participe à .......... les compétitions.",
+    answer: "toutes",
+    options: [
+      "tout",
+      "tous",
+      "toute",
+      "toutes"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Dhawan donne .......... opinion.",
+    answer: "son",
+    options: [
+      "sa",
+      "son",
+      "ses",
+      "la"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Fixez le poste de télévision...........mur !",
+    answer: "au",
+    options: [
+      "à",
+      "au",
+      "à la",
+      "aux"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Je vais ............ Stade.",
+    answer: "au",
+    options: [
+      "à",
+      "au",
+      "à la",
+      "aux"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "Il emmène les élèves ............ France",
+    answer: "en",
+    options: [
+      "a",
+      "en",
+      "à",
+      "à la"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Si vous partiez______cinq heures du matin,vous arriveriez à l'heure.?",
+    answer: "à",
+    options: [
+      "à",
+      "à la",
+      "au",
+      "aux"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Où est-ce que je placerais_____piano ?",
+    answer: "Mon",
+    options: [
+      "la",
+      "Ma",
+      "Mon",
+      "Mes"
+    ]
+  },
+
+ {
+    numb: 9,
+    question: "Vous connaissez mieux______métier !",
+    answer: "votre",
+    options: [
+      "ton",
+      "ta",
+      "vos",
+      "votre"
+    ]
+  },
+
+
+ {
+    numb: 10,
+    question: "Vous voyez,________fils vomit depuis le matin",
+    answer: "Mon",
+    options: [
+      "Les",
+      "Ma",
+      "Mon",
+      "Mes"
+    ]
+  },
+
+ {
+    numb: 11,
+    question: "La famille de Ramki achète_______?",
+    answer: "une maison",
+    options: [
+      "un appartement",
+      "une maison",
+      "une villa",
+      "un studio"
+    ]
+  },
+
+ {
+    numb: 12,
+    question: "_______décore la maison de Ramki?",
+    answer: "L'architecte",
+    options: [
+      "Le père",
+      "La mère",
+      "L'architecte",
+      "Le peintre"
+    ]
+  },
+
+
+ {
+    numb: 13,
+    question: "L'architecte voudrait mettre le canapé______du salon?",
+    answer: "au centre",
+    options: [
+      "au centre",
+      "au coin",
+      "au milieu",
+      "à côté"
+    ]
+  },
+
+ {
+    numb: 14,
+    question: "______voudrait mettre quelques plantes d'intérieur au salon.?",
+    answer: "La mère",
+    options: [
+      "La mère",
+      "L'architecte",
+      "Le fils",
+      "Ramki"
+    ]
+  },
+ {
+    numb: 15,
+    question: "L'architecte suggère de placer les plantes dans le______",
+    answer: "balcon",
+    options: [
+      "salon",
+      "jardin",
+      "balcon",
+      "coin"
+    ]
+  },
+ {
+    numb: 16,
+    question: "On installerait le piano______-du salon?",
+    answer: "au coin",
+    options: [
+      "au coin",
+      "au centre",
+      "au milieu",
+      "à côté"
+    ]
+  },
+
+ {
+    numb: 17,
+    question: "On placerait les statues de_______dans le salon",
+    answer: "bronze",
+    options: [
+      "cuivre",
+      "bronze",
+      "verre",
+      "fer"
+    ]
+  },
+
+   {
+    numb: 18,
+    question: "On pourrait fixer la poste de télévision_______",
+    answer: "au mur",
+    options: [
+      "au mur",
+      "à la table",
+      "au banc",
+      "au plafond"
+    ]
+  },
+
+ {
+    numb: 19,
+    question: "il y a______membres dans la familie de Ramki?.",
+    answer: "quatre",
+    options: [
+      "trois",
+      "quatre",
+      "cinq",
+      "six"
+    ]
+  },
+
+   {
+    numb: 20,
+    question: "La cliente veut voir le docteur pour son_______?",
+    answer: "Enfant",
+    options: [
+      "Mari",
+      "Enfant",
+      "Pere",
+      "Ami"
+    ]
+  },
+
+   {
+    numb: 21,
+    question: "________est malade?",
+    answer: "L'enfant",
+    options: [
+      "L'enfant",
+      "La cliente",
+      "Le père",
+      "La mère"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "La cliente veut un rendez-vous avec le docteur ce_______?",
+    answer: "soir",
+    options: [
+      "matin",
+      "soir",
+      "jour",
+      "midi"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "La cliente veut un rendez-vous avec le_____?",
+    answer: "docteur",
+    options: [
+      "directeur",
+      "secrétaire",
+      "docteur",
+      "patron"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "_______vomit depuis le matin.?",
+    answer: "Le fils",
+    options: [
+      "Le fils",
+      "La fille",
+      "La cliente",
+      "Le garçon"
+    ]
+  },
+
+   {
+    numb: 25,
+    question: "_______a expliqué le problème au docteur",
+    answer: "La secrétaire",
+    options: [
+      "La cliente",
+      "La secrétaire",
+      "La fille",
+      "Le fils"
+    ]
+  },
+
+   {
+    numb: 26,
+    question: "Le docteur examinera l'enfant______?",
+    answer: "sous peu",
+    options: [
+      "sous peu",
+      "le soir",
+      "immédiatement",
+      "plus tard"
+    ]
+  },
+
+   {
+    numb: 27,
+    question: "La cliente veut voir le docteur______",
+    answer: "aujourd'hui",
+    options: [
+      "demain",
+      "aujourd'hui",
+      "lendemain",
+      "immédiatement"
+    ]
+  },
+
+
+  {
+    numb: 28,
+    question: "La cliente remercie_______?",
+    answer: "la secrétaire",
+    options: [
+      "la secrétaire",
+      "le docteur",
+      "l'assistant",
+      "le directeur"
+    ]
+  },
+
+
+   {
+    numb: 29,
+    question: "Le docteur est déjà complet aujourd'hui et il verra seulement demain____?",
+    answer: "matin",
+    options: [
+      "matin",
+      "soir",
+      "après-midi'",
+      "nuit"
+    ]
+  },
+  
+   {
+    numb: 30,
+    question: "La cliente est chez le________",
+    answer: "médecin",
+    options: [
+      "dentiste",
+      "professeur",
+      "mécanicien",
+      "médecin"
+    ]
+  },
+
+
+
+ 
+ 
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
